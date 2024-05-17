5710a3deed66a3af41496368ab8426e6207475c9
pierre schuvey <chromatykretro@gmail.com>
2024-05-17 12:05:58 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -38,41 +38,46 @@ function HomePage(props) {
   return (
     <>
         <div className="homeContainer">
-            <div className={"homeContent"}>
-                <img style={{width: "300px"}} src={"images/logoSofk.png"}/>
-                <div>
-                    <p className={"paragraphHome"}>
-                        Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque année
-                        à l’occasion
-                        de la <span>Journée Internationale des Enfants Disparus</span> pour collecter des dons en faveur
-                        du <span>116 000 Enfants
+            <div className={"firstSectionHome"}>
+                    <img style={{width: "300px"}} src={"images/logoSofk.png"}/>
+                    <div>
+                        <p className={"paragraphHome"}>
+                            Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque
+                            année
+                            à l’occasion
+                            de la <span>Journée Internationale des Enfants Disparus</span> pour collecter des dons en
+                            faveur
+                            du <span>116 000 Enfants
                         Disparus</span>. Ce numéro d’urgence gratuit, disponible <span>24h/24 et 7j7</span>, accompagne
-                        les familles
-                        confrontées à la disparition de leur enfant.
-                    </p>
-                    <p className={"paragraphHome"}>
-                        Créé à l’initiative de <span>Vaykhin</span> en 2021, le <span>Stream On for Kids</span> se
-                        déroule sur Twitch, une
-                        plateforme de vidéos en direct sur laquelle des streamers diffusent du contenu en interagissant
-                        avec leurs
-                        spectateurs. L’événement a permis de collecter près de <span>30 000 €</span> en faveur du numéro
-                        d’urgence
-                        lors de ses trois premières éditions et permet également d’accroitre la notoriété du
-                        numéro <span>116 000</span>
-                        auprès du public.
-                    </p>
-                </div>
+                            les familles
+                            confrontées à la disparition de leur enfant.
+                        </p>
+                        <p className={"paragraphHome"}>
+                            Créé à l’initiative de <span>Vaykhin</span> en 2021, le <span>Stream On for Kids</span> se
+                            déroule sur Twitch, une
+                            plateforme de vidéos en direct sur laquelle des streamers diffusent du contenu en
+                            interagissant
+                            avec leurs
+                            spectateurs. L’événement a permis de collecter près de <span>30 000 €</span> en faveur du
+                            numéro
+                            d’urgence
+                            lors de ses trois premières éditions et permet également d’accroitre la notoriété du
+                            numéro <span>116 000</span>
+                            auprès du public.
+                        </p>
+                    </div>
             </div>
-
-            <h1 className={"titleEdition"}>Liens utiles</h1>
-            <div className={"linkContainerHome"}>
-                <a className="socialLink" target='_blank'
-                   href="https://streamlabscharity.com/teams/@stream-on-for-kids-2024/stream-on-for-kids-2024?member=643451324922470142&l=fr-FR">Faire
-                    un Don</a>
-                <Link className={"linkToTwitch"} to="/Streams">Les lives</Link>
-                <a className="socialLink" target='_blank'
-                   href="https://streamlabscharity.com/teams/@stream-on-for-kids-2024/stream-on-for-kids-2024?member=643451324922470142&l=fr-FR">Faire
-                    un Don</a>
+            <div className={"secondSectionHome"}>
+                <h1 className={"titleEdition"}>Liens utiles</h1>
+                <div className={"linkContainerHome"}>
+                    <a className="socialLink" target='_blank'
+                       href="https://streamlabscharity.com/teams/@stream-on-for-kids-2024/stream-on-for-kids-2024?member=643451324922470142&l=fr-FR">Faire
+                        un Don</a>
+                    <Link className={"linkToTwitch"} to="/Streams">Les lives</Link>
+                    <a className="socialLink" target='_blank'
+                       href="https://streamlabscharity.com/teams/@stream-on-for-kids-2024/stream-on-for-kids-2024?member=643451324922470142&l=fr-FR">Faire
+                        un Don</a>
+                </div>
             </div>
             <h1 className={"titleEdition"}>Editions précédentes</h1>
             <div className={"editionsInfosContainer"}>
