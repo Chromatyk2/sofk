600fc21c5b95caf7cba2efe73ace6d1ecfeacb99
pierre schuvey <chromatykretro@gmail.com>
2024-05-20 09:48:55 +0200

    fix

--- src/component/partners.js
+++ src/component/partners.js
@@ -4,14 +4,16 @@ import '../App.css'
 function Partners(props) {
     return (
         <>
-            <h1 className={"titleEdition"}>Nos partenaires</h1>
-            <div className={"partnerContainer"}>
-                <a href={"https://www.ville-fachesthumesnil.fr/"}><img src={"/images/faches.png"}/></a>
-                <a href={"https://www.tokyosnackbox.com/"}><img src={"/images/tokyo.png"}/></a>
-                <a href={"https://mangadokaze.com/index.php"}><img src={"/images/manga.png"}/></a>
-                <a href={"https://mistertee.fr/"}><img src={"/images/mister.png"}/></a>
-                <a href={"https://www.burgerking.fr/#!"}><img src={"/images/burger.png"}/></a>
-                <a href={"https://www.dominos.fr/"}><img src={"/images/dominos.png"}/></a>
+            <div className={"partnerSection"}>
+                <h1 style={{color:"black"}} className={"titleEdition"}>Nos partenaires</h1>
+                <div className={"partnerContainer"}>
+                    <a href={"https://www.ville-fachesthumesnil.fr/"}><img src={"/images/faches.png"}/></a>
+                    <a href={"https://www.tokyosnackbox.com/"}><img src={"/images/tokyo.png"}/></a>
+                    <a href={"https://mangadokaze.com/index.php"}><img src={"/images/manga.png"}/></a>
+                    <a href={"https://mistertee.fr/"}><img src={"/images/mister.png"}/></a>
+                    <a href={"https://www.burgerking.fr/#!"}><img src={"/images/burger.png"}/></a>
+                    <a href={"https://www.dominos.fr/"}><img src={"/images/dominos.png"}/></a>
+                </div>
             </div>
         </>
     );
