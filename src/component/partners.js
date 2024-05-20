390df204507d8a2612c7c042bf1e2b844356e6f4
pierre schuvey <chromatykretro@gmail.com>
2024-05-20 15:20:50 +0200

    fix

--- src/component/partners.js
+++ src/component/partners.js
@@ -8,12 +8,12 @@ function Partners(props) {
                 <h1 style={{color:"black",fontSize:"20px",padding:"10px"}} className={"titleEdition"}>Nos partenaires</h1>
                 <div className={"partnerContainer"}>
                     <a href={"https://www.ville-fachesthumesnil.fr/"} target={"_blank"}><img src={"/images/faches.png"}/></a>
-                    <div style={{display:"flex",flexFlow:"column",justifyContent:"center"}}>
+                    <div style={{height:"100px",display:"flex",flexFlow:"column",justifyContent:"center"}}>
                         <a href={"https://www.tokyosnackbox.com/"} target={"_blank"}><img src={"/images/tokyo.png"}/></a>
                         <small style={{textAlign:"center"}}>-10 % sur vos commandes avec le code <span style={{fontWeight:"bold"}}>STREAMON10</span><br/> pendant la durée de l'événement</small>
                     </div>
                     <a href={"https://mangadokaze.com/index.php"} target={"_blank"}><img src={"/images/manga.png"}/></a>
-                    <div style={{display:"flex",flexFlow:"column",justifyContent:"center"}}>
+                    <div style={{height:"85px",display:"flex",flexFlow:"column",justifyContent:"center"}}>
                         <a href={"https://mistertee.fr/"} target={"_blank"}><img src={"/images/mister.png"}/></a>
                         <small style={{textAlign:"center"}}>-10 % sur vos commandes avec le code <span style={{fontWeight:"bold"}}>Stream10</span><br/> pendant la durée de
                             l'événement</small>
