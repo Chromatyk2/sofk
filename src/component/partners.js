7cc00cc3150322b2de746ea5bafa2ffe0076f032
pierre schuvey <chromatykretro@gmail.com>
2024-05-20 15:14:02 +0200

    fix

--- src/component/partners.js
+++ src/component/partners.js
@@ -8,14 +8,14 @@ function Partners(props) {
                 <h1 style={{color:"black",fontSize:"20px",padding:"10px"}} className={"titleEdition"}>Nos partenaires</h1>
                 <div className={"partnerContainer"}>
                     <a href={"https://www.ville-fachesthumesnil.fr/"} target={"_blank"}><img src={"/images/faches.png"}/></a>
-                    <div>
+                    <div style={{display:"flex",flexFlow:"column",justifyContent:"center"}}>
                         <a href={"https://www.tokyosnackbox.com/"} target={"_blank"}><img src={"/images/tokyo.png"}/></a>
-                        <small>-10 % sur vos commandes avec le code <bold>STREAMON10</bold> pendant la durée de l'événement</small>
+                        <small>-10 % sur vos commandes avec le code <span style={{fontWeight:"bold"}}>STREAMON10</span><br/> pendant la durée de l'événement</small>
                     </div>
                     <a href={"https://mangadokaze.com/index.php"} target={"_blank"}><img src={"/images/manga.png"}/></a>
-                    <div>
+                    <div style={{display:"flex",flexFlow:"column",justifyContent:"center"}}>
                         <a href={"https://mistertee.fr/"} target={"_blank"}><img src={"/images/mister.png"}/></a>
-                        <small>-10 % sur vos commandes avec le code <bold>Stream10</bold> pendant la durée de
+                        <small>-10 % sur vos commandes avec le code <span style={{fontWeight:"bold"}}>Stream10</span><br/> pendant la durée de
                             l'événement</small>
                     </div>
                     <a href={"https://www.burgerking.fr/#!"} target={"_blank"}><img src={"/images/burger.png"}/></a>
