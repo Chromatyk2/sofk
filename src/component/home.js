0c61d2aa53b1288ea99e378a1b3d4935860b251e
pierre schuvey <chromatykretro@gmail.com>
2024-05-21 13:04:34 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -45,7 +45,7 @@ function HomePage(props) {
         <div className="homeContainer">
             <div className={"firstSectionHome"}>
                 <div className={"homeContent"}>
-                    <img style={{minWidth: "250px", width: "45%"}} src={"images/logoSofk.png"}/>
+                    <img style={{minWidth: "250px", width: "300px", marginBottom:"25px"}} src={"images/logoSofk.png"}/>
                     <div>
                         <p className={"paragraphHome"}>
                             Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque
