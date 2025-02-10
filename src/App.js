b4084bbbc6f8652067be8c0540910780cc7b3590
pierre schuvey <chromatykretro@gmail.com>
2025-02-10 15:46:30 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Partners from "./component/partners";
 import Modal from 'react-modal';
 import StreamsModal from "./component/StreamsModal";
 import Player from "./component/Player";
+import Axios from 'axios';
 function App() {
   const [cookies, setCookie] = useCookies();
   const [modalIsOpen, setIsOpen] = React.useState(false);
