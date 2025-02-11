553b27d0688b9e77cdfe5dd97de78dfa2a7f7647
pierre schuvey <chromatykretro@gmail.com>
2025-02-11 11:22:33 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -69,38 +69,6 @@ function App() {
       })
     }
   }, []);
-  useEffect(() => {
-      Axios.get(
-          'https://api.twitch.tv/helix/teams?name=streamon',
-          {
-            headers: {
-              'Authorization': `Bearer ${cookies.token.access_token}`,
-              'Client-Id': process.env.REACT_APP_CLIENT_ID
-            }
-          }
-      ).then(function (response) {
-        if(response.status == 200) {
-          setTeam(response.data.data[0].users);
-          response.data.data[0].users.map((val, key) => {
-            Axios.get(
-                'https://api.twitch.tv/helix/streams?user_login=' + val.user_name,
-                {
-                  headers: {
-                    'Authorization': `Bearer ${cookies.token.access_token}`,
-                    'Client-Id': process.env.REACT_APP_CLIENT_ID
-                  }
-                }
-            ).then(function (response) {
-              if (response.data.data.length > 0) {
-                setOnStream(oldArrayOn => [...oldArrayOn, {infos: response.data.data}]);
-              } else if (response.data.data.length < 1) {
-                setOffStream(oldArrayOff => [...oldArrayOff, val.user_name]);
-              }
-            })
-          })
-        }
-      })
-  }, [refreash]);
   function openModal() {
     setIsOpen(true);
   }
