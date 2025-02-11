d46c0bb443d392da8ec59f32797d403c9c1e7550
pierre schuvey <chromatykretro@gmail.com>
2025-02-11 11:18:21 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -109,35 +109,35 @@ function App() {
   }
   function reloadEffect(){
     setRefreash(refreash + 1);
+    return(
+        <>
+          <BrowserRouter>
+            <NavBar cookies={cookies}/>
+            <Routes>
+              <Route path="/" element={<HomePage cookies={cookies}/>}/>
+              <Route path="/Streams" element={<StreamOnLayout cookies={cookies}/>}/>
+              <Route path="/Clips" element={<ClipsLayout cookies={cookies}/>}/>
+              <Route path="/Stream" element={<Player cookies={cookies}/>}/>
+            </Routes>
+            {/*<Partners cookies={cookies}/>*/}
+            <Footer cookies={cookies}/>
+            <div className={"buttonStreamsContainer"}>
+              <button onClick={openModal} className={"buttonStreamers"}>Streameur.euses</button>
+              <button className={"buttonStreamers"}>Boutique</button>
+              <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
+                <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
+                  <p style={{color: "white"}}>Streameur.euses</p>
+                  <button style={{color:"white", border:"none", background:"none"}} onClick={closeModal}>X</button>
+                </div>
+                <div className={"streamsModalContainer"}>
+                  <StreamsModal change={closeModal} cookies={cookies} onStream={onStream} offStream={offStream}/>
+                </div>
+              </Modal>
+            </div>
+          </BrowserRouter>
+        </>
+    );
   }
-  return(
-    <>
-      <BrowserRouter>
-        <NavBar cookies={cookies}/>
-        <Routes>
-          <Route path="/" element={<HomePage cookies={cookies}/>}/>
-          <Route path="/Streams" element={<StreamOnLayout cookies={cookies}/>}/>
-          <Route path="/Clips" element={<ClipsLayout cookies={cookies}/>}/>
-          <Route path="/Stream" element={<Player cookies={cookies}/>}/>
-        </Routes>
-        {/*<Partners cookies={cookies}/>*/}
-        <Footer cookies={cookies}/>
-        <div className={"buttonStreamsContainer"}>
-          <button onClick={openModal} className={"buttonStreamers"}>Streameur.euses</button>
-          <button className={"buttonStreamers"}>Boutique</button>
-          <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
-            <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
-              <p style={{color: "white"}}>Streameur.euses</p>
-              <button style={{color:"white", border:"none", background:"none"}} onClick={closeModal}>X</button>
-            </div>
-            <div className={"streamsModalContainer"}>
-              <StreamsModal change={closeModal} cookies={cookies} onStream={onStream} offStream={offStream}/>
-            </div>
-          </Modal>
-        </div>
-      </BrowserRouter>
-    </>
-  );
 }
 
 export default App;
