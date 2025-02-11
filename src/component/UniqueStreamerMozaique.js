9e20c6730fe3e2bcab206cc6544eb0f0cee10672
pierre schuvey <chromatykretro@gmail.com>
2025-02-11 14:47:10 +0100

    fix

--- src/component/UniqueStreamerMozaique.js
+++ src/component/UniqueStreamerMozaique.js
@@ -41,7 +41,7 @@ function UniqueStreamerMozaique(props) {
                         {props.onStream === true ?
                             <>
                                 <div style={{backgroundRepeat:"no-repeat",backgroundImage:"url(https://static-cdn.jtvnw.net/previews-ttv/live_user_"+props.streamer.infos[0].user_name.toLowerCase()+"-496x279.jpg)"}} className={"uniqueStreamerOnlineMozaique"}>
-                                    <button className={"buttonToDisplayStreamMozaique"} value={props.streamer.infos[0].user_name} ></button>
+                                    <button style={{border:"none"}} className={"buttonToDisplayStreamMozaique"} value={props.streamer.infos[0].user_name} ></button>
                                     <div className={"uniqueStreamerProfileMozaique"}>
                                         <img src={user.data[0].profile_image_url}/>
                                         <p>{props.streamer.infos[0].user_name}</p>
@@ -57,7 +57,7 @@ function UniqueStreamerMozaique(props) {
 
                                 <div style={ { backgroundImage: user.data[0].offline_image_url ? 'url('+user.data[0].offline_image_url+')' : 'url('+user.data[0].profile_image_url+')',backgroundRepeat:"no-repeat",backgroundSize:"cover" } }
                                      className={"uniqueStreamerOnlineMozaique"}>
-                                    <button className={"buttonToDisplayStreamMozaique"} value={props.streamer}></button>
+                                    <button style={{border:"none"}} className={"buttonToDisplayStreamMozaique"} value={props.streamer}></button>
                                     <div className={"uniqueStreamerProfileMozaique"}>
                                         <img style={{width: "50px", margin: "0"}} src={user.data[0].profile_image_url}/>
                                         <p>{props.streamer}</p>
