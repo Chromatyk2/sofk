d75d0547915fddf14aebbd89ccf07f85ef213ab0
pierre schuvey <chromatykretro@gmail.com>
2025-02-25 16:45:08 +0100

    fix

--- src/component/UniqueStreamerModal.js
+++ src/component/UniqueStreamerModal.js
@@ -12,15 +12,13 @@ function UniqueStreamerModal(props) {
     useEffect(() => {
         if(props.onStream === true){
             var streamerName = props.streamer.infos[0].user_name;
-            console.log(props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk"))
             props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk").map((val, key) => {
-                setCagnotte(cagnotte + val.donation.converted_amount);
-            })
+                console.log(val.donation)
+            });
         }else{
             var streamerName = props.streamer;
-            console.log(props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk"))
             props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk").map((val, key) => {
-                setCagnotte(cagnotte + val.donation.converted_amount);
+                console.log(val.donation)
             });
         }
         Axios.get(
