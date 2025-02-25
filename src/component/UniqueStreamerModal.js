a8bcf2b4c160ef46454c89bb3eb8c0e0da6ea778
pierre schuvey <chromatykretro@gmail.com>
2025-02-25 16:58:19 +0100

    fix

--- src/component/UniqueStreamerModal.js
+++ src/component/UniqueStreamerModal.js
@@ -12,11 +12,14 @@ function UniqueStreamerModal(props) {
     useEffect(() => {
         if(props.onStream === true){
             var streamerName = props.streamer.infos[0].user_name;
-            console.log(props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk"))
+            props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk").map((val, key) => {
+                console.log(val.donation)
+            });
         }else{
             var streamerName = props.streamer;
-            console.log(props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk"))
-
+            props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == "chromatyk").map((val, key) => {
+                console.log(val.donation)
+            });
         }
         Axios.get(
             'https://api.twitch.tv/helix/users?login='+streamerName,
