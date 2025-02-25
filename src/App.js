340ea3a9398df680f289e109566a329ddee09dc5
pierre schuvey <chromatykretro@gmail.com>
2025-02-25 15:49:50 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -46,15 +46,21 @@ function App() {
             setCharityTeam(response.data);
             Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/members?page=1')
                 .then(function (response) {
-                    setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, response.data.data]);
+                    response.data.data.map((val, key) => {
+                        setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, value]);
+                    })
                     if(response.data.next_page_url !== null){
                         Axios.get(response.data.next_page_url)
                             .then(function (response) {
-                                setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, response.data.data]);
+                                response.data.data.map((val, key) => {
+                                    setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, value]);
+                                })
                                 if(response.data.next_page_url !== null){
                                     Axios.get(response.data.next_page_url)
                                         .then(function (response) {
-                                            setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, response.data.data]);
+                                            response.data.data.map((val, key) => {
+                                                setCharityStreamers(oldArrayCharityStreamers => [...oldArrayCharityStreamers, value]);
+                                            })
                                             setCharityLoad(false);
                                         })
                                 }else{
@@ -68,33 +74,30 @@ function App() {
         })
     }, []);
     useEffect(() => {
-        Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=1')
-            .then(function (response) {
-                response.data.map((val, key) => {
-                    setDonations(oldDonations => [...oldDonations, val]);
+        if(charityLoad === false){
+            Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=1')
+                .then(function (response) {
+                    response.data.map((val, key) => {
+                        setDonations(oldDonations => [...oldDonations, val]);
+                    })
+                    if(response.data.length == 500){
+                        Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=2')
+                            .then(function (response) {
+                                response.data.map((val, key) => {
+                                    setDonations(oldDonations => [...oldDonations, val]);
+                                })
+                                if(response.data.length == 500){
+                                    Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=3')
+                                        .then(function (response) {
+                                            response.data.map((val, key) => {
+                                                setDonations(oldDonations => [...oldDonations, val]);
+                                            })
+                                        })
+                                }
+                            })
+                    }
                 })
-                // if(response.data.length === 500){
-                //     Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=2')
-                //         .then(function (response) {
-                //             response.data.map((val, key) => {
-                //                 setDonations(oldDonations => [...oldDonations, val]);
-                //             })
-                //             if(response.data.length === 500){
-                //                 Axios.get('https://streamlabscharity.com/api/v1/teams/643437249115068091/donations?page=3')
-                //                     .then(function (response) {
-                //                         response.data.map((val, key) => {
-                //                             setDonations(oldDonations => [...oldDonations, val]);
-                //                         })
-                //                         setLoad(false);
-                //                     })
-                //             }else{
-                //                 setLoad(false);
-                //             }
-                //         })
-                // }else{
-                //     setLoad(false);
-                // }
-            })
+        }
     }, [charityLoad]);
     useEffect(() => {
       Axios.post(
