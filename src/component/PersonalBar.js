f823d224a36cf13616991dbfb1b5ee9ecee0981b
pierre schuvey <chromatykretro@gmail.com>
2025-02-26 00:08:57 +0100

    fix

--- src/component/PersonalBar.js
+++ src/component/PersonalBar.js
@@ -1,3 +1,65 @@
+// import React,{useState, useEffect} from 'react';
+// import Axios from 'axios'
+// import {useCookies} from "react-cookie";
+// import {BrowserRouter, Link} from "react-router-dom";
+// import {ca} from "date-fns/locale";
+// import donationGoal from '../donationGoal.json'
+//
+// function PersonalBar(props) {
+//     const [cagnotte, setCagnotte] = useState([]);
+//     const [donation, setDonation] = useState([]);
+//     const customStyles = {
+//         extBar: {
+//             width: "100%",
+//             backgroundColor: "rgb(50, 82, 105)",
+//             position: "relative",
+//             zIndex: 1,
+//             borderRadius: "50px",
+//             margin: 0,
+//             height: "30px"
+//         }
+//     }
+//     useEffect(() => {
+//         const queryParameters = new URLSearchParams(window.location.search)
+//         var streamerName = queryParameters.get("streamer");
+//         props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == streamerName).map((val, key) => {
+//             setCagnotte(oldCagnotte => [...oldCagnotte, val.donation.original_amount]);
+//         });
+//         if (donationGoal[streamerName.toLowerCase()] != undefined) {
+//             setDonation(donationGoal[streamerName.toLowerCase()])
+//         }
+//     }, [])
+//     return (
+//         <>
+//             <p style={{
+//                 fontSize: "13px",
+//                 textAlign: "right",
+//                 margin: 0
+//             }}>
+//                 {donation.length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100).length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100)[0].montant : "Plus de donations goal ! Merci !" : "Plus de donations goal ! Merci !"}
+//             </p>
+//             <div style={customStyles.extBar} className="fullProgressBar">
+//                 <div
+//                     style={{
+//                         width: donation.length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100).length > 0 ? parseFloat(cagnotte.reduce((a, v) => a = a + v, 0) / donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100)[0].montant).toFixed(2) + "%" : "100%" : "100%",
+//                         position: 'relative',
+//                         textWrap: 'nowrap',
+//                         color: '#38617f',
+//                         padding: '15px',
+//                         borderRadius: '50px 50px 50px 50px',
+//                         height: "30px",
+//                         lineHeight: 0,
+//                         backgroundImage: "linear-gradient(180deg,#9ad4de 24%,#fff 155%)"
+//                     }}>
+//                     {cagnotte.reduce((a, v) => a = a + v, 0) / 100} €
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+//
+// export default PersonalBar;
+
 import React,{useState, useEffect} from 'react';
 import Axios from 'axios'
 import {useCookies} from "react-cookie";
@@ -6,8 +68,7 @@ import {ca} from "date-fns/locale";
 import donationGoal from '../donationGoal.json'
 
 function PersonalBar(props) {
-    const [cagnotte, setCagnotte] = useState([]);
-    const [donation, setDonation] = useState([]);
+    const [cagnotte, setCagnotte] = useState(0);
     const customStyles = {
         extBar: {
             width: "100%",
@@ -20,14 +81,14 @@ function PersonalBar(props) {
         }
     }
     useEffect(() => {
-        const queryParameters = new URLSearchParams(window.location.search)
-        var streamerName = queryParameters.get("streamer");
-        props.donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == streamerName).map((val, key) => {
-            setCagnotte(oldCagnotte => [...oldCagnotte, val.donation.original_amount]);
-        });
-        if (donationGoal[streamerName.toLowerCase()] != undefined) {
-            setDonation(donationGoal[streamerName.toLowerCase()])
-        }
+        const interval = setInterval(() =>
+            {
+                setCagnotte(cagnotte + 200)
+            },60000
+        );
+        return () => {
+            clearInterval(interval);
+        };
     }, [])
     return (
         <>
@@ -36,12 +97,12 @@ function PersonalBar(props) {
                 textAlign: "right",
                 margin: 0
             }}>
-                {donation.length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100).length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100)[0].montant : "Plus de donations goal ! Merci !" : "Plus de donations goal ! Merci !"}
+                {donation.filter(item => item.montant >= cagnotte).length > 0 ? cagnotte : "Plus de donations goal ! Merci !"}
             </p>
             <div style={customStyles.extBar} className="fullProgressBar">
                 <div
                     style={{
-                        width: donation.length > 0 ? donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100).length > 0 ? parseFloat(cagnotte.reduce((a, v) => a = a + v, 0) / donation.filter(item => item.montant >= cagnotte.reduce((a, v) => a = a + v, 0) / 100)[0].montant).toFixed(2) + "%" : "100%" : "100%",
+                        width: donation.filter(item => item.montant >= cagnotte).length > 0 ? parseFloat(cagnotte / donation.filter(item => item.montant >= cagnotte)[0].montant).toFixed(2) + "%" : "100%",
                         position: 'relative',
                         textWrap: 'nowrap',
                         color: '#38617f',
@@ -51,7 +112,7 @@ function PersonalBar(props) {
                         lineHeight: 0,
                         backgroundImage: "linear-gradient(180deg,#9ad4de 24%,#fff 155%)"
                     }}>
-                    {cagnotte.reduce((a, v) => a = a + v, 0) / 100} €
+                    {cagnotte} €
                 </div>
             </div>
         </>
@@ -59,3 +120,4 @@ function PersonalBar(props) {
 }
 
 export default PersonalBar;
+
