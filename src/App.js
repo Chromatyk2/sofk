4fdcac86ec27f58f72ccf76b4b12ddac18408055
pierre schuvey <chromatykretro@gmail.com>
2025-03-11 10:15:26 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -108,50 +108,34 @@ function App() {
         };
     }, [charityLoad]);
     useEffect(() => {
-      Axios.post(
-          'https://id.twitch.tv/oauth2/token',
-          {
-            client_id:CLIENT_ID,
-            client_secret:CLIENT_SECRET,
-            grant_type:"client_credentials",
-            redirect_uri:"https://preview--streamonforkids.netlify.app/"
-          }
-      )
-          .then(
-              (result) => {
-                setToken(result.data.access_token);
-                const currentToken = result.data.access_token;
+        Axios.post(
+            'https://id.twitch.tv/oauth2/token',
+            {
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                grant_type: "client_credentials",
+                redirect_uri: "https://preview--streamonforkids.netlify.app/"
+            }
+        ).then(function (response) {
+            console.log(charityStreamers)
+            charityStreamers.map((val, key) => {
                 Axios.get(
-                    'https://api.twitch.tv/helix/teams?name=streamon',
+                    'https://api.twitch.tv/helix/streams?user_login=' + val.user_name,
                     {
-                      headers: {
-                        'Authorization': `Bearer ${currentToken}`,
-                        'Client-Id': process.env.REACT_APP_CLIENT_ID
-                      }
+                        headers: {
+                            'Authorization': `Bearer ${currentToken}`,
+                            'Client-Id': process.env.REACT_APP_CLIENT_ID
+                        }
                     }
                 ).then(function (response) {
-                    console.log(charityStreamers)
-                    charityStreamers.map((val, key) => {
-                      Axios.get(
-                          'https://api.twitch.tv/helix/streams?user_login=' + val.user_name,
-                          {
-                            headers: {
-                              'Authorization': `Bearer ${currentToken}`,
-                              'Client-Id': process.env.REACT_APP_CLIENT_ID
-                            }
-                          }
-                      ).then(function (response) {
-                        if (response.data.data.length > 0) {
-                          setOnStream(oldArrayOn => [...oldArrayOn, {infos: response.data.data}]);
-                        } else if (response.data.data.length < 1) {
-                          setOffStream(oldArrayOff => [...oldArrayOff, val.user_name]);
-                        }
-                      })
-                    })
+                    if (response.data.data.length > 0) {
+                        setOnStream(oldArrayOn => [...oldArrayOn, {infos: response.data.data}]);
+                    } else if (response.data.data.length < 1) {
+                        setOffStream(oldArrayOff => [...oldArrayOff, val.user_name]);
+                    }
                 })
-              }
-          )
-
+            })
+        })
     }, [charityLoad]);
     function openModal() {
         setIsOpen(true);
