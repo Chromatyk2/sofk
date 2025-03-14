f5da99f0dd25fae9a41e00f6e239ceb6ae38ac42
pierre schuvey <chromatykretro@gmail.com>
2025-03-14 10:00:01 +0100

    fix

--- src/component/PersonalBar.js
+++ src/component/PersonalBar.js
@@ -131,14 +131,12 @@ function PersonalBar(props) {
     useEffect(() => {
         if(load === false){
             console.log(cagnotte)
-            console.log(donations)
             const interval = setInterval(() =>
                 {
                     const queryParameters = new URLSearchParams(window.location.search)
                     var streamerName = queryParameters.get("streamer");
                     donations.filter(donation => donation.member != null).filter(donation => donation.member.user.display_name == streamerName).map((val, key) => {
-                        const montant = val.donation.original_amount
-                        setCagnotte(oldCagnotte => [...oldCagnotte, montant]);
+                        setCagnotte(oldCagnotte => [...oldCagnotte, val.donation.original_amount]);
                     });
                     if (donationGoal[streamerName.toLowerCase()] != undefined) {
                         setDonation(donationGoal[streamerName.toLowerCase()])
