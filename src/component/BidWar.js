c4fcd3ed5cf6699fc2e3755eb4191c8ee79d9f26
pierre schuvey <chromatykretro@gmail.com>
2025-04-15 18:11:19 +0200

    fix

--- src/component/BidWar.js
+++ src/component/BidWar.js
@@ -227,11 +227,54 @@ function BidWar(props) {
                                 <p>{bidValueTwo}</p>
                             </div>
                             <div id="bar">
-                                <span className="seg1">F</span>
-                                <span className="seg2">D</span>
-                                <span className="seg3">C</span>
-                                <span className="seg4">B</span>
-                                <span className="seg5">A</span>
+                                <span className="seg1" style={{
+                                    width: parseFloat((valueOne / (parseInt(valueOne) + parseInt(valueTwo))) * 100) + "%",
+                                    backgroundColor: "rgb(252, 194, 73)"
+                                }}>F</span>
+                                <span className="seg2" style={{
+                                    width: parseFloat((valueTwo / (parseInt(valueOne) + parseInt(valueTwo))) * 100) + "%",
+                                    backgroundColor: "red"
+                                }}>D</span>
+                            </div>
+                            <div style={customStyles.extBarInline} className="fullProgressBar">
+                                <div
+                                    className={"intBar"}
+                                    style={{
+                                        width: parseFloat((valueOne / (parseInt(valueOne) + parseInt(valueTwo))) * 100) + "%",
+                                        position: 'relative',
+                                        textWrap: 'nowrap',
+                                        color: 'white',
+                                        padding: '15px',
+                                        borderRadius: '10px 10px 10px 10px',
+                                        height: "37px",
+                                        lineHeight: 0,
+                                        backgroundColor: "rgb(252, 194, 73)",
+                                        textAlign: "left",
+                                        margin: 0
+                                    }}>
+                                </div>
+                                <p style={{
+                                    fontSize: "28px",
+                                    textAlign: "right",
+                                    color: "white",
+                                    position: "absolute",
+                                    left: "12px",
+                                    zIndex: 1,
+                                    top: "0px"
+                                }}>
+                                    {valueOne} €
+                                </p>
+                                <p style={{
+                                    fontSize: "28px",
+                                    textAlign: "right",
+                                    color: "white",
+                                    position: "absolute",
+                                    right: "12px",
+                                    zIndex: 1,
+                                    top: "0px"
+                                }}>
+                                    {valueTwo + " €"}
+                                </p>
                             </div>
                         </div>
                     </div>
