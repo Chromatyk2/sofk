f6f6ed7b3694dada92d9086af645b8b89eb888dc
pierre schuvey <chromatykretro@gmail.com>
2025-05-11 13:15:30 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -19,34 +19,28 @@ import './App.css';
 import TestImg from "./component/testImg";
 import BidWar from "./component/BidWar";
 function App() {
-  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-  const [token, setToken] = useState(null);
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-  const [team, setTeam] = useState([]);
-  const [onStream, setOnStream] = useState([]);
-  const [offStream, setOffStream] = useState([]);
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      background: '#325269'
-    },
-  };
-  function openModal() {
-    setIsOpen(true);
-  }
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-    function refreshStreamers() {
-    setOnStream([]);
-    setOffStream([]);
+    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+    const [token, setToken] = useState(null);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [team, setTeam] = useState([]);
+    const [onStream, setOnStream] = useState([]);
+    const [offStream, setOffStream] = useState([]);
+    const [charityTeam, setCharityTeam] = useState(null);
+    const [charityStreamers, setCharityStreamers] = useState([]);
+    const [load, setLoad] = useState(true);
+    const [charityLoad, setCharityLoad] = useState(true);
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            background: '#325269'
+        },
+    };
     useEffect(() => {
         Axios.get(
             'https://streamlabscharity.com/api/v1/teams/781834327792162028'
@@ -78,7 +72,7 @@ function App() {
                     }else{
                         setCharityLoad(false);
                     }
-            })
+                })
         })
     }, []);
     useEffect(() => {
@@ -158,42 +152,39 @@ function App() {
     return(
         <>
             <div className={window.location.pathname != "/OoqZvHhdnIrOGL" ? "globalDiv" : "globalDivTransparent"}>
-                    <BrowserRouter>
-                        <NavBar/>
-                        <div className={"buttonStreamsContainer"}>
-                            <button onClick={openModal} className={"buttonStreamers"}>Streameur.euses</button>
-                            {/*<button className={"buttonStreamers"}>Boutique</button>*/}
-                            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}
-                                   contentLabel="Example Modal">
-                                <div style={{display: "flex", justifyContent: "space-between", alignItems: "baseline"}}>
-                                    <p style={{color: "white"}}>Streameur.euses</p>
-                                    <button style={{color: "white", border: "none", background: "none"}}
-                                            onClick={closeModal}>X
-                                    </button>
-                                </div>
-                                <div className={"streamsModalContainer"}>
-                                    <StreamsModal charityStreamers={charityStreamers} change={closeModal} onStream={onStream} offStream={offStream} token={token}/>
-                                </div>
-                            </Modal>
-                        </div>
-                        <Routes>
-                            <Route path="/" element={<HomePage />}/>
-                            <Route path="/Streams"
-                                   element={<StreamOnLayout token={token} offStream={offStream} onStream={onStream} change={refresh}/>}/>
-                            <Route path="/Clips" element={<ClipsLayout change={refresh} team={charityStreamers} token={token}/>}/>
-                            <Route path="/Stream" element={<Player change={refresh} team={charityStreamers} token={token}/>}/>
-                            <Route path="/OoqZvHhdnIrOGL" element={<PersonalBar charityStreamers={charityStreamers} onStream={false} token={token} />}/>
-                            <Route path="/OoqZvHhdnIrOGLB" element={<BidWar charityStreamers={charityStreamers} onStream={false} token={token} />}/>
-                            <Route path="/OoqZvHhdnIrOGLs" element={<TestImg />}/>
+                <BrowserRouter>
+                    <NavBar/>
+                    <div className={"buttonStreamsContainer"}>
+                        <button onClick={openModal} className={"buttonStreamers"}>Streameur.euses</button>
+                        {/*<button className={"buttonStreamers"}>Boutique</button>*/}
+                        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}
+                               contentLabel="Example Modal">
+                            <div style={{display: "flex", justifyContent: "space-between", alignItems: "baseline"}}>
+                                <p style={{color: "white"}}>Streameur.euses</p>
+                                <button style={{color: "white", border: "none", background: "none"}}
+                                        onClick={closeModal}>X
+                                </button>
+                            </div>
+                            <div className={"streamsModalContainer"}>
+                                <StreamsModal charityStreamers={charityStreamers} change={closeModal} onStream={onStream} offStream={offStream} token={token}/>
+                            </div>
+                        </Modal>
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<HomePage />}/>
+                        <Route path="/Streams"
+                               element={<StreamOnLayout token={token} offStream={offStream} onStream={onStream} change={refresh}/>}/>
+                        <Route path="/Clips" element={<ClipsLayout change={refresh} team={charityStreamers} token={token}/>}/>
+                        <Route path="/Stream" element={<Player change={refresh} team={charityStreamers} token={token}/>}/>
+                        <Route path="/OoqZvHhdnIrOGL" element={<PersonalBar charityStreamers={charityStreamers} onStream={false} token={token} />}/>
+                        <Route path="/OoqZvHhdnIrOGLB" element={<BidWar charityStreamers={charityStreamers} onStream={false} token={token} />}/>
+                        <Route path="/OoqZvHhdnIrOGLs" element={<TestImg />}/>
 
-                        </Routes>
-                        {/*<Partners cookies={cookies}/>*/}
-                        <Footer/>
-                    </BrowserRouter>
+                    </Routes>
+                    {/*<Partners cookies={cookies}/>*/}
+                    <Footer/>
+                </BrowserRouter>
             </div>
         </>
     );
 }
-}
-
-export default App;
