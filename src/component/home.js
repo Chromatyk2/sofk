0b0bd969dfe9b61dc1a6d272edcafe545308dda8
pierre schuvey <chromatykretro@gmail.com>
2025-05-15 11:40:23 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -7,7 +7,13 @@ function HomePage(props) {
             <div className="homeContainer">
                 <div className={"homeContent"}>
                     <img style={{width: "250px", marginBottom: "25px"}} src={"images/logoSofk.png"}/>
-                    <div style={{maxWidth:"650px", display:"block", margin:"auto", marginTop:"60px", marginBottom:"60px"}}>
+                    <div style={{
+                        maxWidth: "650px",
+                        display: "block",
+                        margin: "auto",
+                        marginTop: "60px",
+                        marginBottom: "60px"
+                    }}>
                         <p className={"paragraphHome"}>
                             Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque
                             année
@@ -33,7 +39,7 @@ function HomePage(props) {
                             auprès du public.
                         </p>
                     </div>
-                    <div style={{marginTop:"30px", marginBottom:"150px"}}>
+                    <div style={{marginTop: "30px", marginBottom: "150px"}}>
                         <h1 className={"titleEdition"}>Editions précédentes</h1>
                         <div className={"editionsInfosContainer"}>
                             <div className={"editionsContainer"}>
@@ -53,6 +59,38 @@ function HomePage(props) {
                                 <p className={"totalEdition"}>2 800 €</p>
                             </div>
                         </div>
+                    </div>
+                    <div style={{
+                        maxWidth: "650px",
+                        display: "block",
+                        margin: "auto",
+                        marginTop: "60px",
+                        marginBottom: "60px"
+                    }}>
+                        <p className={"paragraphHome"}>
+                            Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque
+                            année
+                            à l’occasion
+                            de la <span>Journée Internationale des Enfants Disparus</span> pour collecter des dons en
+                            faveur
+                            du <span>116 000 Enfants
+                        Disparus</span>. Ce numéro d’urgence gratuit, disponible <span>24h/24 et 7j7</span>, accompagne
+                            les familles
+                            confrontées à la disparition de leur enfant.
+                        </p>
+                        <p className={"paragraphHome"}>
+                            Créé à l’initiative de <span>Vaykhin</span> en 2021, le <span>Stream On for Kids</span> se
+                            déroule sur Twitch, une
+                            plateforme de vidéos en direct sur laquelle des streamers diffusent du contenu en
+                            interagissant
+                            avec leurs
+                            spectateurs. L’événement a permis de collecter près de <span>30 000 €</span> en faveur du
+                            numéro
+                            d’urgence
+                            lors de ses trois premières éditions et permet également d’accroitre la notoriété du
+                            numéro <span>116 000</span>
+                            auprès du public.
+                        </p>
                     </div>
                 </div>
             </div>
