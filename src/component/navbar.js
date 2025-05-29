8fbc4bea5f79812a5052972328673bf43af4d113
pierre schuvey <chromatykretro@gmail.com>
2025-05-30 01:57:14 +0200

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -28,7 +28,7 @@ function NavBar(props) {
                         <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/Clips">Clips</Link>
                         <Link className="navLink linkFromNav" target={"_blank"} to="/https://thepixelwar.fr/">Pixel War</Link>
                         <Link style={{color:"#fcc249"}} onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/Asso">Le 116 000</Link>
-                        <Link style={{position: "absolute", right: "50px"}} target={"_blank"} to={"https://www.instagram.com/streamonforkids/"}><img style={{width: "30px"}} src={InstaSVG} alt=""/></Link>
+                        <a style={{position: "absolute", right: "50px"}} target={"_blank"} href={"https://www.instagram.com/streamonforkids/"}><img style={{width: "30px"}} src={InstaSVG} alt=""/></a>
 
                     </Nav>
                 </Navbar.Collapse>
